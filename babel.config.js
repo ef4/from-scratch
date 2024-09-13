5654c7c99b79e65bc338acd8a1f4a4758b915805
Edward Faulkner <edward@eaf4.com>
2024-09-13 01:31:20 +0100

    add template support

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@ const { babelCompatSupport } = require("@embroider/compat/babel");
 
 module.exports = {
   plugins: [
+    ['babel-plugin-ember-template-compilation'],
     ...babelCompatSupport()
   ],
   generatorOpts: {
--- vite.config.mjs
+++ vite.config.mjs
@@ -1,12 +1,16 @@
 import { defineConfig } from "vite";
 import { babel } from '@rollup/plugin-babel';
-import { optimizeDeps, resolver } from '@embroider/vite';
+import { optimizeDeps, resolver, templateTag } from '@embroider/vite';
 
 export default defineConfig(() => {
   return {
     plugins: [
-      babel({ babelHelpers: 'bundled' }),
+      babel({ 
+        babelHelpers: 'bundled',
+        extensions: ['.js', '.gjs'],
+       }),
       resolver(),
+      templateTag(),
     ],
     optimizeDeps: optimizeDeps(),
   };
