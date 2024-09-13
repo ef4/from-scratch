d522971f73d19b195b49c6d8ac326cb8ed17245b
Edward Faulkner <edward@eaf4.com>
2024-09-13 02:10:18 +0100

    fill in router and config/environment

---
+++ app/router.js
@@ -0,0 +1,9 @@
+import EmberRouter from '@ember/routing/router';
+import config from './config/environment';
+
+export default class Router extends EmberRouter {
+  location = config.locationType;
+}
+
+Router.map(function () {
+});
